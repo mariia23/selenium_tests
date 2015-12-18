827029aa83f52cca13363211ffd5cbaf6925d01a
mariia <you@example.com>
2015-12-18 22:50:30 +0200

    add second test

--- test_sample.py
+++ test_sample.py
@@ -1,12 +1,26 @@
+# -*- coding: utf-8 -*-
+
+
 from selenium.webdriver import PhantomJS
 from time import sleep
+import random 
 
 
-def test_phantom():
+def test_redirect_yandex_ua():
+    current_url = get_property('current_url')
+    assert current_url.startswith('https://www.yandex.ua/')
+
+
+def test_yandex_title():
+    title = get_property('title')
+    assert title.startswith(u'Яндекс')
+
+
+def get_property(property_name):
     phantom = PhantomJS(service_args=['--ssl-protocol=any'])
     phantom.get('https://yandex.ru')
     sleep(15)
-    current_url = phantom.current_url
-    phantom.save_screenshot('phantom.png')
+    properti = getattr(phantom, property_name)
+    phantom.save_screenshot('phantom-{}.png'.format(random.random()))
     phantom.quit()
-    assert current_url.startswith('https://www.yandex.ua/')
+    return properti
