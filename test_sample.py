793206dfbd00e0b6df9d9b9acb3f071285bcdc61
mariia <you@example.com>
2015-12-23 00:03:12 +0200

    add custom phantom class

--- test_sample.py
+++ test_sample.py
@@ -3,7 +3,24 @@
 import random
 
 import pytest
+from selenium.common.exceptions import TimeoutException
 from selenium.webdriver import PhantomJS
+
+
+
+class MutantPhantom(PhantomJS):
+
+    def get(self, url):
+        try:
+            super(MutantPhantom, self).get(url)
+        except TimeoutException:
+            pass
+
+    def refresh(self):
+        try:
+            super(MutantPhantom, self).refresh()
+        except TimeoutException:
+            pass
 
 
 def test_redirect_yandex_ua(phantom):
@@ -21,7 +38,7 @@ def test_refresh_yandex_ua(phantom):
 
 @pytest.yield_fixture
 def phantom(request):
-    phantom = PhantomJS(service_args=['--ssl-protocol=any'])
+    phantom = MutantPhantom(service_args=['--ssl-protocol=any'])
     phantom.set_page_load_timeout(30)
     phantom.get('https://yandex.ru')
 
